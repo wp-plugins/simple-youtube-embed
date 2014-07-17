4eb339fe70740e1f299fe331c5c64204e97c25ae
naa986 <naa986@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-17 14:01:26 +0000

    .committing version 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/simple-youtube-embed/trunk@950307 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ main.php
@@ -0,0 +1,108 @@
+<?php
+/*
+Plugin Name: Simple YouTube Embed
+Version: 1.0.1
+Plugin URI: http://noorsplugin.com/2014/07/17/simple-youtube-embed-plugin/
+Author: naa986
+Author URI: http://noorsplugin.com/
+Description: Embed YouTube video beautifully on your WordPress site
+*/
+
+if(!defined('ABSPATH')) exit;
+if(!class_exists('SIMPLE_YOUTUBE_EMBED'))
+{
+    class SIMPLE_YOUTUBE_EMBED
+    {
+        var $plugin_version = '1.0.1';
+        var $plugin_url;
+        var $plugin_path;
+        function __construct()
+        {
+            define('SIMPLE_YOUTUBE_EMBED_VERSION', $this->plugin_version);
+            define('SIMPLE_YOUTUBE_EMBED_SITE_URL',site_url());
+            define('SIMPLE_YOUTUBE_EMBED_URL', $this->plugin_url());
+            define('SIMPLE_YOUTUBE_EMBED_PATH', $this->plugin_path());
+            $this->plugin_includes();
+            add_action( 'wp_enqueue_scripts', array( &$this, 'plugin_scripts' ), 0 );
+        }
+        function plugin_includes()
+        {
+            if(is_admin( ) )
+            {
+                //add_filter('plugin_action_links', array(&$this,'add_plugin_action_links'), 10, 2 );
+            }
+            //add_action('admin_menu', array( &$this, 'add_options_menu' ));
+            add_action('wp_head', 'simple_youtube_video_embed_js');
+            add_filter('embed_oembed_html', 'simple_youtube_video_embed', 10, 3);
+        }
+        function plugin_scripts()
+        {
+            if (!is_admin()) 
+            {
+                wp_enqueue_script('jquery');
+                wp_register_script('waitforimages', SIMPLE_YOUTUBE_EMBED_URL.'/jquery.waitforimages.min.js', array('jquery'), SIMPLE_YOUTUBE_EMBED_VERSION);
+                wp_enqueue_script('waitforimages');
+                wp_register_script('prettyembed', SIMPLE_YOUTUBE_EMBED_URL.'/jquery.prettyembed.min.js', array('jquery'), SIMPLE_YOUTUBE_EMBED_VERSION);
+                wp_enqueue_script('prettyembed');
+                wp_register_script('fitvids', SIMPLE_YOUTUBE_EMBED_URL.'/jquery.fitvids.js', array('jquery'), SIMPLE_YOUTUBE_EMBED_VERSION);
+                wp_enqueue_script('fitvids');
+            }
+        }
+        function plugin_url()
+        {
+            if($this->plugin_url) return $this->plugin_url;
+            return $this->plugin_url = plugins_url( basename( plugin_dir_path(__FILE__) ), basename( __FILE__ ) );
+        }
+        function plugin_path(){ 	
+            if ( $this->plugin_path ) return $this->plugin_path;		
+            return $this->plugin_path = untrailingslashit( plugin_dir_path( __FILE__ ) );
+        }
+        function add_plugin_action_links($links, $file)
+        {
+            if ( $file == plugin_basename( dirname( __FILE__ ) . '/main.php' ) )
+            {
+                $links[] = '<a href="options-general.php?page=simple-youtube-embed-settings">Settings</a>';
+            }
+            return $links;
+        }
+
+        function add_options_menu()
+        {
+            if(is_admin())
+            {
+                add_options_page('Simple YouTube Embed Settings', 'Simple YouTube Embed', 'manage_options', 'simple-youtube-embed-settings', array(&$this, 'display_options_page'));
+            }
+        }
+    }
+    $GLOBALS['simple_youtube_embed'] = new SIMPLE_YOUTUBE_EMBED();
+}
+
+function simple_youtube_video_embed($html, $url, $attr) 
+{
+    $yt_pattern = "/^(?:http(?:s)?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:(?:watch)?\?(?:.*&)?v(?:i)?=|(?:embed|v|vi|user)\/))([^\?&\"'>]+)/";
+    if(preg_match($yt_pattern,$url,$matches))
+    {
+        $youtube_id = $matches[1];
+        $embed_code = <<<EOT
+        <div class="pretty-embed" data-pe-videoid="$youtube_id" data-pe-fitvids="true"></div>  
+EOT;
+        $html = $embed_code;
+    }
+    return $html; 
+}
+
+function simple_youtube_video_embed_js()
+{
+    $output = <<<EOT
+    <script type="text/javascript" charset="utf-8">
+        /* <![CDATA[ */
+        jQuery(document).ready(function($){
+            $(function(){
+                $().prettyEmbed({ useFitVids: true });
+            });
+        });
+        /* ]]> */
+        </script>
+EOT;
+    echo $output;
+}
---
+++ readme.txt
@@ -0,0 +1,57 @@
+=== Simple YouTube Embed ===
+Contributors: naa986
+Donate link: http://noorsplugin.com/
+Tags: video, embed, youtube, embed videos, embed youtube, embedding youtube, fluid, html5, iPad, iphone, mobile, simple, simple youtube, video plugin, wordpress youtube embed, youtube embed, youtube player, youtube plugin, responsive, shortcode, wordpress, you tube, youtube player
+Requires at least: 3.0
+Tested up to: 3.9
+Stable tag: 1.0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Embed YouTube video beautifully on your WordPress site
+
+== Description ==
+
+Simple YouTube Embed plugin is the easiest way to embed YouTube videos on WordPress. The plugin is very lightweight and easy to use.
+
+= Feature =
+
+* Pretty YouTube video embed
+* Responsive and mobile friendly 
+
+= Simple YouTube Embed Usage =
+
+Create a new post/page and enter your YouTube video URL to embed it. For example:
+
+`http://www.youtube.com/watch?v=Vpg9yizPP_g`
+
+For documentation please visit the [Simple YouTube Embed](http://noorsplugin.com/2014/07/17/simple-youtube-embed-plugin/) plugin page
+
+== Installation ==
+
+1. Go to the Add New plugins screen in your WordPress Dashboard
+1. Click the upload tab
+1. Browse for the plugin file (simple-youtube-embed.zip) on your computer
+1. Click "Install Now" and then hit the activate button
+
+== Frequently Asked Questions ==
+
+= Can this plugin be used to embed YouTube video? =
+
+Yes.
+
+= Can my YouTube videos be responsive? =
+
+Yes.
+
+== Screenshots ==
+
+1. Simple YouTube Embed Demo
+
+== Upgrade Notice ==
+none
+
+== Changelog ==
+
+= 1.0.1 =
+* First commit
